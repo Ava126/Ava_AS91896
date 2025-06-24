98f25170248c7a767822d092c3c3a264b88d1b61
Ava126 <chus@wsc.school.nz>
2025-06-24 12:11:30 +1200

    Delete Ava_AS91896-1.py

--- Ava_AS91896-1.py
+++
@@ -1,6 +0,0 @@
-name=input("What's your name?")
-print("Hello, "+ name)
-print(name +" is a nice name!")
-age =int(input("How old are you?"))
-age += 10
-print(("You are {} years old in ten years").format(age))
