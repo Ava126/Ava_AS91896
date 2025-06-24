369624bc48c7b90bf706951ef21ecdb96a51a889
Ava126 <chus@wsc.school.nz>
2025-06-24 12:12:05 +1200

    Add files via upload

---
+++ Ava_AS91896.py
@@ -0,0 +1,6 @@
+name=input("What's your name?")
+print("Hello, "+ name)
+print(name +" is a nice name!")
+age =int(input("How old are you?"))
+age += 10
+print(("You are {} years old in ten years").format(age))
